5aadee52ef3647613a96417e1f58ad163ca2b2bf
Philip Huang <philiphuang2@gmail.com>
2022-10-04 20:38:43 -0700

    add example row for the calendar

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
-    console.log(moment().format("dddd, MMMM Do YYYY"));
-})
+// $(document).ready(function () {
+//     console.log(moment().format("dddd, MMMM Do YYYY"));
+// })
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
