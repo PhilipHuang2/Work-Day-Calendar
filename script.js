a01cb93265182076e654af741a030c8e5e48a52f
Philip Huang <philiphuang2@gmail.com>
2022-10-05 23:29:40 -0700

    wrote event listeners to add description into local Storage

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// $(document).ready(function () {
-//     console.log(moment().format("dddd, MMMM Do YYYY"));
-// })
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
 var timeLogContainer = $('.container');
 var currentHour = parseInt(moment().format("k"));
 var timeLogTime;
-console.log(currentHour);
 // timeLogHolder.append(timeLogRow); 
 for (const timeRow of timeLogContainer.children()) {
     // the time Log Time and set it to 24 hour format
@@ -14,7 +10,6 @@ for (const timeRow of timeLogContainer.children()) {
     // given the current time, set each description box to either past, present or future
     if(timeLogTime < 9)
         timeLogTime = timeLogTime + 12;
-    console.log(timeLogTime);
     if(timeLogTime < currentHour)
         $(timeRow).children(".description").addClass("past");
     else if( timeLogTime == currentHour)
@@ -22,3 +17,32 @@ for (const timeRow of timeLogContainer.children()) {
     else 
         $(timeRow).children(".description").addClass("future");
 }
+
+// for each text area, set the val() to whatever is pulled from storage
+var day = moment().format("D-M-YYYY");
+var stor;
+if(stor = localStorage.getItem(day))
+{
+    stor = JSON.parse(stor);
+    
+}
+
+
+//create an event listener at each button
+// that takes the data in text area and stores it
+// in local storage
+$("button").on("click", function() {
+     
+    
+    if(stor = localStorage.getItem(day))
+        stor = JSON.parse(stor);
+    else
+        stor = {};
+    var section = $(this).siblings("div").text();
+    stor[section] = $(this).siblings("textarea").val()
+    localStorage.setItem(day, JSON.stringify(stor));
+    // console.log(moment().format("D-M-YYYY"));
+    // console.log($(this).siblings("textarea").val());
+})
+
+
