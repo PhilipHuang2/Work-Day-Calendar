162039984e4fdb569aa14d641f9fae396ad45372
Philip Huang <philiphuang2@gmail.com>
2022-10-05 18:44:40 -0700

    dynamically adjust background of textarea based on current time

--- script.js
+++ script.js
@@ -3,50 +3,36 @@
 // })
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-var workDay = [
-  "9 AM",
-  "10 AM",
-  "11 AM",
-  "12 PM",
-  "1 PM",
-  "2 PM",
-  "3 PM",
-  "4 PM",
-  "5 PM",
-];
-
-var timeLogHolder = $('.container');
-var timeLogRow;
-var timeLogColumn;
+var timeLogContainer = $('.container');
+var currentHour = parseInt(moment().format("k"));
+var timeLogTime;
+console.log(currentHour);
 // timeLogHolder.append(timeLogRow); 
-for (const day of workDay) {
-    // creating row for to hold time log
-    timeLogRow = $("<div class='row my-2'></div>");
-    // creating hour column
-    timeLogColumn = $("<div class='hour col-1'></div>");
-    timeLogColumn.text(day);
-    //append column to row
-    timeLogRow.append(timeLogColumn);
-    // description column
-    timeLogColumn = $("<div class='description col-10 past'></div>");
-    timeLogRow.append(timeLogColumn);
-    timeLogColumn = $("<div class='saveBtn col-1 d-flex align-items-center justify-content-center'><i class='fas fa-save'></i></div>");
-    timeLogRow.append(timeLogColumn);
+for (const timeRow of timeLogContainer.children()) {
+    // the time Log Time and set it to 24 hour format
+    timeLogTime = parseInt($(timeRow).children(".hour").text().split(" ")[0]);
+    // given the current time, set each description box to either past, present or future
+    if(timeLogTime < 9)
+        timeLogTime = timeLogTime + 12;
+    console.log(timeLogTime);
+    if(timeLogTime < currentHour)
+        $(timeRow).children(".description").addClass("past");
+    else if( timeLogTime == currentHour)
+        $(timeRow).children(".description").addClass("present");
+    else 
+        $(timeRow).children(".description").addClass("future");
+    // // creating row for to hold time log
+    // timeLogRow = $("<div class='row my-2'></div>");
+    // // creating hour column
+    // timeLogColumn = $("<div class='hour col-1'></div>");
+    // timeLogColumn.text(day);
+    // //append column to row
+    // timeLogRow.append(timeLogColumn);
+    // // description column
+    // timeLogColumn = $("<div class='description col-10 past'></div>");
+    // timeLogRow.append(timeLogColumn);
+    // timeLogColumn = $("<div class='saveBtn col-1 d-flex align-items-center justify-content-center'><i class='fas fa-save'></i></div>");
+    // timeLogRow.append(timeLogColumn);
 
-    timeLogHolder.append(timeLogRow);
-}
-
-{
-  /* <div class="row my-2">
-        <div class="hour col-1 ">
-            9AM
-
-        </div>
-        <div class="description col-10 past">
-
-        </div>
-        <div class="saveBtn col-1 d-flex align-items-center justify-content-center">
-          <i class="fas fa-save"></i>
-        </div>
-      </div> */
+    // timeLogHolder.append(timeLogRow);
 }
