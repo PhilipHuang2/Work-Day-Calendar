67bfe5abecc28df9b46873e5b1b5abb1d5bb9b1e
Philip Huang <philiphuang2@gmail.com>
2022-10-05 18:54:43 -0700

    converted description field to text areas

--- script.js
+++ script.js
@@ -21,18 +21,4 @@ for (const timeRow of timeLogContainer.children()) {
         $(timeRow).children(".description").addClass("present");
     else 
         $(timeRow).children(".description").addClass("future");
-    // // creating row for to hold time log
-    // timeLogRow = $("<div class='row my-2'></div>");
-    // // creating hour column
-    // timeLogColumn = $("<div class='hour col-1'></div>");
-    // timeLogColumn.text(day);
-    // //append column to row
-    // timeLogRow.append(timeLogColumn);
-    // // description column
-    // timeLogColumn = $("<div class='description col-10 past'></div>");
-    // timeLogRow.append(timeLogColumn);
-    // timeLogColumn = $("<div class='saveBtn col-1 d-flex align-items-center justify-content-center'><i class='fas fa-save'></i></div>");
-    // timeLogRow.append(timeLogColumn);
-
-    // timeLogHolder.append(timeLogRow);
 }
